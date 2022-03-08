62d8ae5b0e4bc2716c0a718c9f506f4febb5af31
thephotogenicbug <navinkumarachari@gmail.com>
2022-03-08 21:53:03 +0530

    08-03-2022 : Build
    Registration , Login , and Get all tickets from newticket table SQL Query

--- server.js
+++ server.js
@@ -20,20 +20,71 @@ const mydatabase = mysql.createConnection({
 });
 mydatabase.connect();
 
-app.post('/newticket', (req,res) =>{
-    var service =     req.body.service;
-    var service_submenu = req.body.service_submenu;
-    var textbox      = req.body.textbox;
-    var image     = req.body.image;
-    var ticketid = uniqid();
-    var sql = "insert into newticket(service, service_submenu, textbox, image, ticketid)values('"+service+"', '"+service_submenu+"', '"+textbox+"', '"+image+"', '"+ticketid+"')";
-    mydatabase.query(sql, function(error, rows, fields){
+// Register
+app.post("/register", function(req , res){
+    var name = req.body.uname;
+    var password = req.body.password;
+    var mobile = req.body.mobile;
+    var email = req.body.email;
+    var sql = "insert into users(name, password, mobile, email) values('"+name+"', '"+password+"', '"+mobile+"', '"+email+"')";
+    mydatabase.query( sql , function(error , rows, fields){
         if(error) throw error
-        res.status(201).json({
-            success:true,
-            message:"New Ticket Generated Successfully....!"
-        })
+       res.status(201).json({
+         success: true,
+         message: "Registration Successful...!",
+       });
     })
+});
+
+// Login
+app.post('/login', (req,res) =>{
+  var email = req.body.email;
+  var password = req.body.password;
+  var sql = "select * from users where email='" +email+ "' and password='" +password+ "'";
+  mydatabase.query(sql, function(error, rows, fields){
+    if(error) throw error
+    if(rows.length > 0){
+      res.send(rows);
+      res.end()
+    } else{
+      res.send({"id":""});
+      res.end();
+    }
+  })
+})
+
+// Post New Ticket
+app.post('/newticket', (req,res) =>{
+    var projectname = req.body.projectname;
+    var date = req.body.date;
+    var duedate = req.body.duedate;
+    var notes = req.body.notes;
+    var budget = req.body.budget;
+    var status = req.body.status;
+    var image   = req.body.image;
+    var ticketid = req.body.ticketid;
+    var clientid = req.body.clientid;
+    var sql = "insert into newticket(projectname, date, duedate, notes, budget, status, image, ticketid, clientid ) values('"+projectname+"', '"+date+"', '"+duedate+"', '"+notes+"', '"+budget+"', '"+status+"', '"+image+"', '"+ticketid+"', '"+clientid+"')";
+      mydatabase.query(sql, function(error, rows, fields){
+        if(error) throw error
+         res.status(201).json({
+           success: true,
+           message: "New Ticket Generated Successfully....!",
+         });
+      })
+   
+})
+
+// Get Ticket
+app.get('/getallticket', (req,res) =>{
+  var sql = "select * from newticket order by id desc";
+  mydatabase.query(sql, function(error, rows, fields){
+    if(error) throw error
+    res.status(201).json({
+      success:true,
+      rows
+    })
+  })
 })
 
 app.listen(process.env.PORT, () => {
